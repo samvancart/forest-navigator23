e2a643149744fc89fde38680baedd5b7ac858127
samvancart <sam02cart@gmail.com>
2023-08-25 13:57:57 +0300

    df wide to long

--- main.py
+++ main.py
@@ -27,56 +27,95 @@ def kelvin_to_celsius(df, vars):
         df[var] = df[var]-273.15
     return df
 
+def prebas_out_var_to_long_form(var, speciesID=2, nyears=38):
+    df_path = f'data/csv/prebas_out/{var}.csv'
+    df = pd.read_csv(df_path)
+    # DROP THINNING INFO (CHECK YEARS)
+    df.drop(df.iloc[:, nyears+1:], inplace=True, axis=1)
+    # DROP FIRST COLUMN
+    df.drop(df.iloc[:,:1], inplace=True, axis=1)
+    # STRIP COLUMN NAMES FROM X1.stand to X1
+    df.rename(columns=lambda x: x.split('.')[0].strip(), inplace=True)
+    df['climID'] = df.index+1
+    df['speciesID'] = speciesID
+    df = pd.wide_to_long(df, stubnames='X', i=['climID', 'speciesID'], j='year')
+    df.rename(columns={'X': var}, inplace=True)
+    return df
+
+def join_dataframes(vars):
+    dfs = []
+    new = pd.DataFrame()
+    for i, var in enumerate(vars):
+        df = prebas_out_var_to_long_form(var)
+        dfs.append(df)
+        if i == 0:
+            new = dfs[0]
+            dfs=[]
+        else:
+            new = new.join(dfs[0])
+            dfs = []
+
+    return new
+
+
 def main():
+    # vars = ['ba','dbh','gross_growth','h','n','npp','v']
+    # new = join_dataframes(vars)
+    # print(new)
+    # path = f'data/csv/prebas_out/prebas_out_vars.csv'
+    # uf.write_df_to_csv(new, path, index=True)
+
     prebas_path = f'data/csv/climate/historical_climate_data.csv'
     df = pd.read_csv(prebas_path, parse_dates=['time'])
-    df = df.rename(columns={'PlgID' : 'siteID', 'XLON':'lon', 'YLAT':'lat', 'rsds':'qq', 'hurs': 'rh' , 'tas' : 'tair', 'prcp':'precip', 'tasmax': 't_max', 'tasmin': 't_min'})
-    # GET CO2 VALUES
-    co2_csv_path = f'data/csv/climate/co2_annual_1850_2021.csv'
-    co2_df = pd.read_csv(co2_csv_path)
-    co2_df = co2_df.loc[co2_df.year.isin(df.time.dt.year)]
-    co2_df.drop(co2_df.tail(4).index, inplace=True)
-    new = pd.DataFrame({'year':range(2018,2100)})
-    new['CO2'] = 405.22
-    co2_df = pd.concat([co2_df, new])
-    df['year'] = df.time.dt.year
-    df = df.merge(co2_df)
-    df.drop(['year'], axis=1)
-    # ASSIGN CLIMIDS
-    df['climID'] = df.groupby(['siteID']).ngroup()+1
-    # GET RSS FROM QQ
-    df = df.assign(rss = lambda x: x['qq'] *0.0864)
-    # GET PAR FROM RSS
-    df = df.assign(par = lambda x: x['rss'] *0.44*4.56)
-    # REARRANGE
-    df = df.drop(columns=['rh', 'rss'])
-    cols = ['time', 'siteID', 'climID', 'lat', 'lon', 'tair', 'precip', 'par', 'vpd', 't_min', 't_max', 'CO2']
-    df = uf.rearrange_df(df, cols)
-    # CONVERT [kg.m-2.s-1] to mm/d (86400 seconds in day)
-    df['precip'] = df['precip'] * 86400
-    # CONVERT TEMPERATURES FROM KELVIN TO CELSIUS
-    df = kelvin_to_celsius(df, ['tair', 't_max', 't_min'])
-    # df['CO2'] = 380
-    df = df.drop(['t_min', 't_max', 'lat', 'lon'], axis=1)
-    df = df.rename(columns={"par":"PAR","tair":"TAir","vpd":"VPD","precip":"Precip"})
-    cols = ["time","climID","PAR","TAir","VPD","Precip","CO2"]
-    df = uf.rearrange_df(df, cols)
-    path = f'data/csv/climate/historical_prebas.csv'
-    uf.write_df_to_csv(df, path)
+    print(df)
+    
+    # df = df.rename(columns={'PlgID' : 'siteID', 'XLON':'lon', 'YLAT':'lat', 'rsds':'qq', 'hurs': 'rh' , 'tas' : 'tair', 'prcp':'precip', 'tasmax': 't_max', 'tasmin': 't_min'})
+    # # GET CO2 VALUES
+    # co2_csv_path = f'data/csv/climate/co2_annual_1850_2021.csv'
+    # co2_df = pd.read_csv(co2_csv_path)
+    # co2_df = co2_df.loc[co2_df.year.isin(df.time.dt.year)]
+    # co2_df.drop(co2_df.tail(4).index, inplace=True)
+    # new = pd.DataFrame({'year':range(2018,2100)})
+    # new['CO2'] = 405.22
+    # co2_df = pd.concat([co2_df, new])
+    # df['year'] = df.time.dt.year
+    # df = df.merge(co2_df)
+    # df.drop(['year'], axis=1)
+    # # ASSIGN CLIMIDS
+    # df['climID'] = df.groupby(['siteID']).ngroup()+1
+    # # GET RSS FROM QQ
+    # df = df.assign(rss = lambda x: x['qq'] *0.0864)
+    # # GET PAR FROM RSS
+    # df = df.assign(par = lambda x: x['rss'] *0.44*4.56)
+    # # REARRANGE
+    # df = df.drop(columns=['rh', 'rss'])
+    # cols = ['time', 'siteID', 'climID', 'lat', 'lon', 'tair', 'precip', 'par', 'vpd', 't_min', 't_max', 'CO2']
+    # df = uf.rearrange_df(df, cols)
+    # # CONVERT [kg.m-2.s-1] to mm/d (86400 seconds in day)
+    # df['precip'] = df['precip'] * 86400
+    # # CONVERT TEMPERATURES FROM KELVIN TO CELSIUS
+    # df = kelvin_to_celsius(df, ['tair', 't_max', 't_min'])
+    # # df['CO2'] = 380
+    # df = df.drop(['t_min', 't_max', 'lat', 'lon'], axis=1)
+    # df = df.rename(columns={"par":"PAR","tair":"TAir","vpd":"VPD","precip":"Precip"})
+    # cols = ["time","climID","PAR","TAir","VPD","Precip","CO2"]
+    # df = uf.rearrange_df(df, cols)
+    # path = f'data/csv/climate/historical_prebas.csv'
+    # uf.write_df_to_csv(df, path)
 
-    # WRITE TRAN FILES
-    prebas_path = f'data/csv/climate/historical_prebas.csv'
-    df = pd.read_csv(prebas_path, parse_dates=['time'])
-    vars = ['PAR', 'TAir', 'Precip', 'VPD', 'CO2']
-    for v in vars:
-        path = f'data/csv/climate/tran/{v}_tran.csv'
-        var_tran = var_to_prebas_tran(df, v)
-        print(var_tran)
-        uf.write_df_to_csv(var_tran, path)
+    # # WRITE TRAN FILES
+    # prebas_path = f'data/csv/climate/historical_prebas.csv'
+    # df = pd.read_csv(prebas_path, parse_dates=['time'])
+    # vars = ['PAR', 'TAir', 'Precip', 'VPD', 'CO2']
+    # for v in vars:
+    #     path = f'data/csv/climate/tran/{v}_tran.csv'
+    #     var_tran = var_to_prebas_tran(df, v)
+    #     print(var_tran)
+    #     uf.write_df_to_csv(var_tran, path)
 
   
 
-    print(df)
+    # print(df)
 
 
 
